79c5d74ac2c7778c45f54095eb9e995d8215ad30
emrekgn <emre.akkaya@agem.com.tr>
2017-03-22 11:58:39 +0300

    Separate update site script from the built process

---
+++ scripts/generate-update-site.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+###
+# This script builds the project and exports the Lider Console product for Linux (x32, x64), Windows (x32, x64) and MacOSX.
+#
+# Exported products can be found under /tmp/lider-console
+###
+set -e
+
+pushd $(dirname $0) > /dev/null
+PRJ_ROOT_PATH=$(dirname $(pwd -P))
+popd > /dev/null
+echo "Warning: In order to generate update site, all plugin projects needs to be built. Otherwise this script will fail!"
+echo "Project path: $PRJ_ROOT_PATH"
+
+# Generate third-party dependencies
+echo "Generating third-party dependencies..."
+cd "$PRJ_ROOT_PATH"/lider-console-dependencies
+mvn clean p2:site
+echo "Generated third-party dependencies."
+
+# Start jetty server for Tycho to use generated dependencies
+echo "Starting server for Tycho..."
+mvn jetty:run &
+J_PID=$!
+echo "Started server."
+
+# Build project
+echo "Building lider-console project..."
+cd "$PRJ_ROOT_PATH"
+mvn clean install -DskipTests
+echo "lider-console project built successfully."
+
+# After exporting products, kill jetty server process
+echo "Shutting down server..."
+kill $J_PID
+echo "Server shut down."
+
+EXPORT_PATH=/tmp/lider-console
+echo "Export path: $EXPORT_PATH"
+
+# Copy resulting files
+echo "Copying exported lider-console products to $EXPORT_PATH..."
+mkdir -p "$EXPORT_PATH"
+cp -rf "$PRJ_ROOT_PATH"/lider-console-products/target/products/. "$EXPORT_PATH"
+cp -rf "$PRJ_ROOT_PATH"/lider-console-update-site/target/*.zip "$EXPORT_PATH"
+echo "Copied exported lider-console products."
+
+echo "Built finished successfully!"
+echo "Files can be found under: $EXPORT_PATH"
